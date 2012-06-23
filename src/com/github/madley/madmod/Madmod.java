b6f04ebe369afc569fa9369bc29895ad90058694
Thelron <thelron.github@yahoo.com>
2012-06-23 15:18:32 -0400

    Initial project shell
    Gives us a base class to play around with and makes sure we don't need
    any extra files

---
+++ src/com/github/madley/madmod/Madmod.java
@@ -0,0 +1,16 @@
+package com.github.madley.madmod;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Madmod extends JavaPlugin {
+
+	public void onEnable(){
+		
+		getLogger().info("Madmod says madmod");
+	}
+	public void onDisable(){
+		
+		getLogger().info("Madmod says NUUUUUU");
+	}
+
+}
